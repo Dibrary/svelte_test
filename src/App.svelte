811cdf3576b3e8db3907ac26d4ac3b343748c221
Dibrary <kkdh828@hanmail.net>
2022-02-25 09:18:07 +0900

    svelte 기본 연습

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,35 @@
 <script>
-	export let name;
-</script>
+	import Header from './header.svelte'; // svelte 파일 불러와서 html요소 배치
+	import Btn from './button.svelte';
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	let count = 0; // 상태값 (나는 변수라고 부르는데.. )
+	const handleClick = () => {
+		count += 1;
+		if(count >10) {
+			count = 0;
 		}
 	}
-</style>
+
+	let user = {loggedIn:true};
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+
+</script>
+
+<Header/> <!-- 여기서 html요소 배치한다 -->
+
+<!-- <button>그냥 버튼</button> -->
+<Btn {handleClick}{count}/> <!--버튼 컴포넌트로 함수, 변수 보냄. (어떻게 사용할 지는 Btn 파일 안으로 가봐야 알 수 있음.)-->
+<p>{count}</p>
+
+{#if user.loggedIn}
+	<button on:click={toggle}>
+		Log out
+	</button>
+{/if}
+{#if !user.loggedIn}
+	<button on:click={toggle}>
+		Log in
+	</button>
+{/if}
---
+++ src/button.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let handleClick; // App 에서 보낸 것(함수든 변수든) 을 받아올 수 있음.
+    export let count;
+</script>
+
+<button on:click={handleClick}>그냥 버튼{count}</button> <!--버튼 클릭 시 함수 실행-->
